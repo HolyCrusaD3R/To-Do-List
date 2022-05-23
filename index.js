647ef057f5fe77499303db4045a3e6a2d8d34dfe
Luka Rekhviashvili <rekhviashvili.luka125@gmail.com>
2022-05-24 00:23:56 +0400

    fix bugs regarding with empty input and text going over the border

--- index.js
+++ index.js
@@ -8,8 +8,20 @@ var s=[];
 // need to create a function to update(remove, probably add listener or smth)
 
 add.addEventListener("click", (e) => {
-    s.push(input.value);
-    //console.log(s);
-    display.innerHTML += "<div class='goal'>" + s.length.toString(10) + ". " + s[s.length-1] + "</div><br>";
+    
+    if(input.value.length==0)
+        return;
+    
+    let st=input.value;
+    for(let i=20;i<st.length;i+=20)
+    {
+        if(st[i]==' ') st=[st.slice(0,i),"<br>",st.slice(i)].join('');
+            else st=[st.slice(0,i),"-<br>",st.slice(i)].join('');
+    }
+    console.log(st);   
+    
+    s.push(st);
+
+    display.innerHTML += "<li id=" + s.length + ">" + s[s.length-1] + "</li><br>";
     input.value="";
 });
